afcdf74426a2af64fa18ae61e31ede914b3283fc
Eelis Takala <eelis.takala@gmail.com>
2021-04-29 22:32:05 +0300

    Improve logging for testing

--- SuperconductingCoilQuenchCircuit/run.sh
+++ SuperconductingCoilQuenchCircuit/run.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+if [ "$#" == "0" ]
+then
+	echo "Run name not set for logging purposes. You can give it with: ./run.sh run_name"
+	logname=""
+else
+	echo "Run name set to $1."
+	logname="$1-"
+fi
+
+DATE=$(date +%s)
+LOGFILE="$logname$DATE-run.log"
 echo " "
 # ElmerGrid 8 2 *.unv -out MESH -autoclean
-rm ./RESU/coil_*
+echo "Run the coil powering: coil.sif" | tee $LOGFILE
+rm ./RESU/coil_* | tee -a $LOGFILE
 echo "*** ElmerSolver ***"
-ElmerSolver coil.sif
-python ./Python/plot_ramp.py
-eog ./Figures/ramp.png
-ElmerSolver coil_extraction.sif
-python ./Python/plot_all.py
-eog ./Figures/all_current.png
-eog ./Figures/all_voltage.png
+ElmerSolver coil.sif | tee -a $LOGFILE
+python ./Python/plot_ramp.py | tee -a $LOGFILE
+eog ./Figures/ramp.png &
+echo "Run the coil extraction: coil_extraction.sif" | tee -a $LOGFILE
+ElmerSolver coil_extraction.sif | tee -a $LOGFILE
+python ./Python/plot_all.py | tee -a $LOGFILE
+eog ./Figures/all_current.png &
+eog ./Figures/all_voltage.png &
+mv $LOGFILE log/
 
 
 
