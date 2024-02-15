67c44d8b66e3624f2741434ec28be747de49a749
Kevin Bersch <kevin.bersch@monumo.com>
2024-02-15 10:02:09 +0000

    Connect coils in same phase in series

--- PermanentMagnetMachine2D/circuit.py
+++ PermanentMagnetMachine2D/circuit.py
@@ -33,19 +33,22 @@ def create_circuit_delta(dir_out: Path):
     current_1 = I("I_1", 1, 2, peak_current)
     current_2 = I("I_2", 1, 2, peak_current)
 
-    phase_0 = ElmerComponent("Phase_0", 2, 1, 1, [2, 3], 1)
-    phase_1 = ElmerComponent("Phase_1", 2, 1, 2, [6, 7], 1)
-    phase_2 = ElmerComponent("Phase_2", 2, 1, 3, [4, 5], 1)
-    phases = [phase_0, phase_1, phase_2]
+    phase_0_0 = ElmerComponent("Phase_0_0", 2, 3, 1, [2], 1)
+    phase_0_1 = ElmerComponent("Phase_0_1", 3, 1, 2, [3], 1)
+    phase_1_0 = ElmerComponent("Phase_1_0", 2, 3, 3, [6], 1)
+    phase_1_1 = ElmerComponent("Phase_1_1", 3, 1, 4, [7], 1)
+    phase_2_0 = ElmerComponent("Phase_2_0", 2, 3, 5, [4], 1)
+    phase_2_1 = ElmerComponent("Phase_2_1", 3, 1, 6, [5], 1)
+    phases = [phase_0_0, phase_0_1, phase_1_0, phase_1_1, phase_2_0, phase_2_1]
     for phase in phases:
         n_turns = 35
         resistance = 0.8 / float(n_turns)
         phase.stranded(n_turns, resistance)
 
     # store components in array components = [comp1, comp2,...] - do not remove
-    c[1].components.append([current_0, phase_0])
-    c[2].components.append([current_1, phase_1])
-    c[3].components.append([current_2, phase_2])
+    c[1].components.append([current_0, phase_0_0, phase_0_1])
+    c[2].components.append([current_1, phase_1_0, phase_1_1])
+    c[3].components.append([current_2, phase_2_0, phase_2_1])
 
     fname_out = "circuit.definition"
     path_out = dir_out / fname_out
